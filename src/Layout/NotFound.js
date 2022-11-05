926ee87e0ea3ba73d8016116663f1d379204c8d1
unknown <ndiayembayesamb@gmail.com>
2022-11-04 18:13:58 -0700

    adding button

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -1,8 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { HomeButton } from "./components/homepagecomponent/DecksButton";
 function NotFound() {
   return (
     <div className="NotFound">
+      <Link to='/'>
+        <HomeButton />
+
+      </Link>
       <h1>Not Found</h1>
     </div>
   );
